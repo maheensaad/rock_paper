01fa2671ca84edbbbf9a27c3ddb4a953731d990a
Maheen <maheensaad1995@gmail.com>
2022-03-17 02:01:42 +0500

    Prompt added and the game is repeated five times

--- rps.js
+++ rps.js
@@ -9,18 +9,40 @@ function playRound(playerSelection, computerSelection)
     if(computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
     return "It's a draw";
     } 
-    else if(computerSelection.toLowerCase() === "paper") {
+    else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
     return "You lose";
     }
-    else if(computerSelection.toLowerCase() === "scissors") {
+    else if(computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
     return "You win";
     }
-    else{
+    else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
+        return "You win";
+    }
+    else if(computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
+        return "You lose";
+    }
+    else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
+        return "You lose";
+    }
+    else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
+        return "You win";
+    }
+    else {
         return "beats me";
     }
+
 }
 
-const playerSelection = "rock";
+
 const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Do you choose 'Rock', 'Paper', or 'Scissors'?");
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        
+     }
+}
+console.log(game());
