ed7b28351d51cb1a93b4d6a8eac95137ef0a79b7
Maheen <maheensaad1995@gmail.com>
2022-03-17 01:50:56 +0500

    Add changed rps.js

--- rps.js
+++ rps.js
@@ -3,4 +3,24 @@ function computerPlay() {
     
     return myArray[Math.floor(Math.random()*myArray.length)];
 }
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) 
+{
+    if(computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+    return "It's a draw";
+    } 
+    else if(computerSelection.toLowerCase() === "paper") {
+    return "You lose";
+    }
+    else if(computerSelection.toLowerCase() === "scissors") {
+    return "You win";
+    }
+    else{
+        return "beats me";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
